ebcb0b321669032e676d9143c7eeeb0243db457a
Richard Clayton <richardclayton@peachjar.com>
2020-03-22 22:33:54 -0700

    Add some debug info

--- src/requireJson.ts
+++ src/requireJson.ts
@@ -5,8 +5,11 @@ const readFileAsync = promisify(readFile)
 
 export default async function requireJson(path: string): Promise<Record<string, any>> {
     try {
+        console.log('Looking up file in path:', path)
         const contents = await readFileAsync(path, 'utf-8')
-        return JSON.parse(contents)
+        const json = JSON.parse(contents)
+        console.log(json)
+        return json
     } catch (error) {
         throw error
     }
